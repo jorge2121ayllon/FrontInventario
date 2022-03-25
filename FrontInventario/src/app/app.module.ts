c060a5b4abecf9e7dfe55648fb8e7fa779429b38
educato-gonzales <clau.yeri@gmail.com>
2022-03-25 16:04:18 -0400

    fff

--- FrontInventario/src/app/app.module.ts
+++ FrontInventario/src/app/app.module.ts
@@ -20,7 +20,7 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import { NgxMatFileInputModule } from '@angular-material-components/file-input';
+//import { NgxMatFileInputModule } from '@angular-material-components/file-input';
 
 //Componentes de material
 import {MatProgressBarModule} from '@angular/material/progress-bar';
