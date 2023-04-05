01ffa3dcefa9dd1359b394a56a41a5de74e9fb11
Leonardo Ismael Perez Torrez <leonardoismaelpereztorrez@gmail.com>
2023-04-04 22:39:43 -0400

    cambiando las redirecciones del api

--- FrontInventario/src/environments/environment.prod.ts
+++ FrontInventario/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  appUrl:'https://fashsporttarija.online/'
+  appUrl:'https://obispadotarija-001-site2.etempurl.com/'
 };
--- FrontInventario/src/environments/environment.ts
+++ FrontInventario/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  appUrl:'https://fashsporttarija.online/'
+  appUrl:'https://obispadotarija-001-site2.etempurl.com/'
 };
 
 /*
